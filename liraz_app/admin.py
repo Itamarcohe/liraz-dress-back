edb7a9d0300c9a572aca6f2c8aba1b7de7329d5e
Itamar Cohen <Itamar330@gmail.com>
2022-08-19 20:29:10 +0300

    pushing

---
+++ liraz_app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from .models import *
+
+admin.site.register(Dress)
+admin.site.register(Lead)
+# Register your models here.
---
+++ liraz_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LirazAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'liraz_app'
